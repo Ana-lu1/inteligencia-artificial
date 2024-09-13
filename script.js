81344277c875a5c138d144eebed5a054014b2dad
Ana Luisa <106676458+Ana-lu1@users.noreply.github.com>
2024-09-13 09:00:43 -0300

    Update script.js
    Projeto final

--- script.js
+++ script.js
@@ -10,63 +10,63 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Aumentar a desigualdade social, concentrando poder e riqueza em poucas mãos.",
-                afirmacao: "Se preocupa com a situação de classes baixas diante dessa nova realidade."
+                afirmacao: "Você se preocupa com a situação de classes baixas diante dessa nova realidade."
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Criar novas oportunidades de trabalho e melhorar a qualidade de vida da maioria das pessoas.",
+                afirmacao: "Você acredita que a IA pode se tornar uma ferramenta eficaz em nosso meio."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Qual será o maior desafio para a implementação da IA em larga escala?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Garantir a segurança e privacidade dos dados.",
+                afirmacao: "Com constantes ataques hackers em sistemas brasileiros, você demonstra um certo receio em qustão da segurança dessa nova ferramenta no Brasil."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Desenvolver algoritimos imparciais e livres de vieses.",
+                afirmacao: "Como esta é uma ferramenta no qual foi criada por humanos, você teme que ela seja desenvolvida com bases políticas e padrões de discriminação e preconceito."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "A regulamentação da IA será:",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Essencial para evitar o uso malicioso da tecnologia.",
+                afirmacao: "Com esta inovação. surge a preocupação de como será usado essa nova ferramenta na socieda e onde uma regulamentação que garanta não só a proteção do usuario mas do uso correto da IA."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Um obstáculo ao desenvolvimento e inovação no setor.",
+                afirmacao: "Com base nos atuais projetos do governo. surje a preocupação de regulamentações que desfavorece o povo brasileiro de classes baixas."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "A IA deve ser desenvolvida com o objetivo de:",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Maximizar o lucro das empresas.",
+                afirmacao: "Essa ferramenta seria criada para beneficiar apenas grandes empresas em seus lucros e produções."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Beneficiar a sociedade como um todo.",
+                afirmacao: "Essa ferramenta seria criada para beneficiar a população em geral, independente de ser da classe baixa ou da elite."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Por fim, você usaria a IA em sua rotina?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Sim!",
+                afirmacao: "Visando tudo isso, você acredita no potencial e futuro da IA em nossa sociedade atual."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Não!",
+                afirmacao: "Visando tudo isso, você ainda tem receio do potencial e futuro da IA em nossa sociedade evitando o uso."
             }
         ]
     },
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Ao final...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
